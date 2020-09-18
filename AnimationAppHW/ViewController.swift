5a8a531000b27bfebb721ba69be17d19613eb665
Ivan Savkov <ivansavkov1387@gmail.com>
2020-09-18 10:39:37 +0300

    Initial Commit

---
+++ AnimationAppHW/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AnimationAppHW
+//
+//  Created by Иван on 9/18/20.
+//  Copyright © 2020 Ivan Savkov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
